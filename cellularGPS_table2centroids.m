301efffb9ec96d62e833393654e195b51634a65a
karhohs <gandalfisarockstar@gmail.com>
2014-10-17 11:19:33 -0400

    Change to table2centroids

--- cellularGPS_table2centroids.m
+++ cellularGPS_table2centroids.m
@@ -8,8 +8,13 @@ maxrows = p.Results.maxrows;
 Centroids = CentroidTimeseries(max(matrixData.timepoint), maxrows);
 
 uniqueTimepoints = unique(matrixData.timepoint);
+[~,indexes] = ismember({'centroid_col', 'centroid_row'}, matrixData.Properties.VariableNames);
+if(sum(indexes > 0) < 2)
+    fprintf('The table does not contain centroid_col and centroid_row fields. Centroids will not be allocated\n');
+    return;
+end
 for i=1:length(uniqueTimepoints)
-    subMatrix = matrixData{matrixData.timepoint == uniqueTimepoints(i),1:2};
+    subMatrix = matrixData{matrixData.timepoint == uniqueTimepoints(i),indexes};
     Centroids.insertCentroids(uniqueTimepoints(i), subMatrix);
 end
 
