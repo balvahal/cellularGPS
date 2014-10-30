b066396fe5ab6e7da997597c98044ffe411ea4e0
ReyesJose <josereyes@fas.harvard.edu>
2014-10-30 17:34:53 -0400

    LAP2

--- cellularGPSTracking_LAP.m
+++ cellularGPSTracking_LAP.m
@@ -1,3 +1,23 @@
-function simplifiedMatrix = cellularGPSTracking_LAP(costMatrix)
+function [winningMatrix, cost] = cellularGPSTracking_LAP(costMatrix, i, LAPmatrix)
+validColumns = find(costMatrix(i,:) > -1);
+winningMatrix = LAPmatrix;
+cost = Inf;
+for j=1:length(validColumns)
+    tempLAPMatrix = LAPmatrix;
+    tempLAPMatrix(i,j) = 1;
     
+    tempCostMatrix = costMatrix;
+    tempCostMatrix(:,j) = -1;
+    
+    if(i==size(costMatrix,1))
+        competingMatrix = tempLAPMatrix;
+        competingCost = sum(tempCostMatrix(competingMatrix));
+    else
+        [competingMatrix, competingCost] = cellularGPSTracking_LAP(tempCostMatrix, i+1, tempLAPMatrix);
+    end
+    if(competingCost < cost)
+        winningMatrix = competingMatrix;
+        cost = competingCost;
+    end
+end
 end
