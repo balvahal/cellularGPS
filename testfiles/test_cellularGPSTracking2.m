dfc6c16395988670b12be9376deb2258ebaafed1
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2014-11-06 18:20:25 -0500

    cost matrix construction expanded :jack_o_lantern:
    going in the right direction

--- testfiles/test_cellularGPSTracking2.m
+++ testfiles/test_cellularGPSTracking2.m
@@ -1,6 +1,6 @@
 %% parameters
 %
-movementThreshold = 50;
+movementThreshold = 30;
 %% Load centroids
 %
 [mfilepath,~,~] = fileparts(mfilename('fullpath')); %finds the path to this script
@@ -15,14 +15,14 @@ centroidCell = cell(size(mytime));
 for i = 1:length(mytime)
     centroidCell{i} = sortrows(cenTable(cenTable.timepoint == mytime(i),:),{'centroid_col','centroid_row'},{'ascend','ascend'});
 end
-trackCounter = height(centroidCell{1})+1;
-trackID = transpose(1:height(centroidCell{1}));
-trackCostMax = ones(height(myCentroid),1)*movementThreshold^2;
 myCentroid = centroidCell{1};
+trackCounter = height(myCentroid)+1;
+trackID = transpose(1:height(myCentroid));
+trackCostMax = 0;
 myCentroid.trackID = trackID;
-myCentroid.trackCost = trackCostMax;
+myCentroid.trackCost = zeros(height(myCentroid),1);
 centroidCell{1} = myCentroid;
-%%%
+%% cost matrix
 % the cost matrix for the first timepoint cannot take into account any
 % prior tracking or position information
 for i = 2:length(mytime) %loop 1
@@ -31,46 +31,58 @@ for i = 2:length(mytime) %loop 1
     centroid2lp1 = centroidCell{i};
     Nlp1 = centroid2lp1{:,{'centroid_col','centroid_row'}};
     distM = cellularGPSTracking_distanceMatrix(Mlp1,Nlp1);
-    distM(distM>movementThreshold) = -1;
-    costM11 = distM;
+    %%% costM11
+    %
+    costM11 = distM.^2;
+    costM11(costM11>movementThreshold^2) = -1;
+    %%%
+    % this is to initialize the trackCostMax
+    if i==2 && any(costM11(:)~=-1)
+        trackCostMax = prctile(costM11(costM11~=-1),80);
+    end
+    %%% costM12
+    %
     costM12 = ones(size(Mlp1,1),size(Mlp1,1))*-1;
     for j = 1:size(Mlp1,1) %loop 1 in loop 1
-            costM12(j,j) = trackCostMax(j);
+        costM12(j,j) = trackCostMax;
     end
+    %%% costM21
+    %
     costM21 = ones(size(Nlp1,1),size(Nlp1,1))*-1;
-    costNoLink = max(costM12(:));
     for j = 1:size(Nlp1,1)
-        costM21(j,j) = costNoLink;
+        costM21(j,j) = trackCostMax;
     end
-    costM = [costM11,costM12;costM21,ones(size(Nlp1,1),size(Mlp1,1))*costNoLink];
+    %%% costM22
+    % The minimum value of the costM11 at the values of the transpose of
+    % costM11.
+    costM22 = transpose(costM11);
+    costM22(costM22 ~= -1) = min([min(costM11(costM11 ~= -1)),min(diag(costM12)),min(diag(costM21))]);
+    %%% assemble the cost matrix
+    %
+    costM = [costM11,costM12;costM21,costM22];
     costM(costM == -1) = Inf;
     [ROWSOL,COST,v,u,rMat] = lapjv(costM);
     %%
     %
     trackID = zeros(size(Nlp1,1),1);
-    trackCostOld = trackCostMax;
-    trackCostMax  = zeros(size(Nlp1,1),1);
     trackCost = zeros(size(Nlp1,1),1);
-for j = 1:size(Mlp1,1)
-    if ROWSOL(j) <= size(Nlp1,1)
-        trackID(ROWSOL(j)) = centroid1lp1.trackID(j);
-        trackCost(ROWSOL(j)) = costM(j,ROWSOL(j));
-            if costM(j,ROWSOL(j)) > trackCostOld(j)/1.05 || i == 1
-                trackCostMax(ROWSOL(j)) = 1.05*costM(j,ROWSOL(j)); 
-            else
-                trackCostMax(ROWSOL(j)) = trackCostOld(j);
-            end
+    for j = 1:size(Mlp1,1)
+        if ROWSOL(j) <= size(Nlp1,1)
+            trackID(ROWSOL(j)) = centroid1lp1.trackID(j);
+            trackCost(ROWSOL(j)) = costM11(j,ROWSOL(j));
+        end
     end
-end
-trackCostMax(trackCostMax == 0) = median(trackCostMax(trackCostMax ~= 0));
-for j = find(trackID == 0)
+    if max(trackCost) > trackCostMax
+        trackCostMax = max(trackCost);
+    end
+    for j = transpose(find(trackID == 0))
         trackID(j) = trackCounter;
         trackCounter = trackCounter + 1;
-        trackCost(j) = trackCostMax(j);
-end
-centroid2lp1.trackID = trackID;
-centroid2lp1.trackCost = trackCost;
-centroidCell{i} = centroid2lp1;
+        trackCost(j) = costM21(j,j);
+    end
+    centroid2lp1.trackID = trackID;
+    centroid2lp1.trackCost = trackCost;
+    centroidCell{i} = centroid2lp1;
 end
 %% plot solution
 %
@@ -84,11 +96,14 @@ i = 0;
 for j = 1:length(trackID)
     mylogical = masterCentroid.trackID == trackID(j);
     tracklength(j) = sum(mylogical);
-%     if tracklength(j) < 50
-%         continue
-%     else
-%         i = i+1
-%     end
+        if tracklength(j) == 1
+            myrow = masterCentroid.centroid_row(mylogical);
+            mycol = masterCentroid.centroid_col(mylogical);
+            mytime = masterCentroid.timepoint(mylogical);
+            output = sortrows([mytime,mycol,myrow]);
+            plot(output(:,2),output(:,3),'o','Color',[rand rand rand],'LineWidth',1.5);
+            continue
+        end
     myrow = masterCentroid.centroid_row(mylogical);
     mycol = masterCentroid.centroid_col(mylogical);
     mytime = masterCentroid.timepoint(mylogical);
@@ -98,3 +113,4 @@ end
 hold off
 myax = gca;
 set(myax,'ydir','reverse')
+sum(tracklength > 50)
